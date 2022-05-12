deb4e80c41426d542d164ad51505946245b41a81
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-05-12 03:57:24 +0200

    Update test to check template pages retrieval

--- Practice.App/Practice.App.Tests/Templates/Queries/GetTemplateSectionQueryTests.cs
+++ Practice.App/Practice.App.Tests/Templates/Queries/GetTemplateSectionQueryTests.cs
@@ -21,16 +21,43 @@ namespace Practicer.App.Tests
                 new TemplateSignature("1", "1"),
                 new TemplateSignature("2", "2"),
                 new TemplateSignature("3", "3"),
+                new TemplateSignature("4", "4"),
+                new TemplateSignature("5", "5"),
             };
 
         private static readonly TemplateSignatureList SignatureList = 
             new TemplateSignatureList(
                 AppConfiguration.TemplateSignatureListID,
-                new List<string>() { "2", "3", "1" }
+                new List<string>() { "2", "3", "1", "5", "4" }
+            );
+
+        private static readonly TemplatePage Page =
+            new TemplatePage(
+                "ID",
+                "PageName",
+                new List<TemplatePage>()
+                {
+                    new TemplatePage("ID_Child1", "PageName_Child1", "2"),
+                    new TemplatePage(
+                        "ID_Child2",
+                        "PageName_Child2",
+                        new List<TemplatePage>()
+                        {
+                            new TemplatePage("ID_Child21", "PageName_Child21", "5")
+                        },
+                        "3",
+                        isTemplateExpanded: true,
+                        isPageExpanded: false
+                    ),
+                    new TemplatePage("ID_Child3", "PageName_Child3", "4"),
+                },
+                "1",
+                isTemplateExpanded: true,
+                isPageExpanded: false
             );
 
         [Test]
-        public async Task GivenQuery_WhenHandle_ThenVMCorrect()
+        public async Task Handle()
         {
             // Arrange
             var signatureRepository = Substitute.For<IRepository<TemplateSignature>>();
@@ -43,8 +70,14 @@ namespace Practicer.App.Tests
                 .GetBy(AppConfiguration.TemplateSignatureListID)
                 .Returns(Result.SuccessTask(SignatureList));
 
+            var templatePageRepository = Substitute.For<IRepository<TemplatePage>>();
+            templatePageRepository
+                .GetBy(AppConfiguration.TemplatePageRootID)
+                .Returns(Result.SuccessTask(Page));
+
             // Act
-            var result = await CreateHandler(signatureRepository, signatureListRepository).Handle(Query);
+            var result = await CreateHandler(
+                signatureRepository, signatureListRepository, templatePageRepository).Handle(Query);
             var vm = result.Get();
 
             // Assert
@@ -53,10 +86,16 @@ namespace Practicer.App.Tests
             Assert.AreEqual(vm.Signatures[0].ID, "2");
             Assert.AreEqual(vm.Signatures[1].ID, "3");
             Assert.AreEqual(vm.Signatures[2].ID, "1");
+            Assert.AreEqual(vm.Signatures[3].ID, "5");
+            Assert.AreEqual(vm.Signatures[4].ID, "4");
+            Assert.AreEqual(vm.Pages[0].ID, "ID_Child1");
+            Assert.AreEqual(vm.Pages[1].ID, "ID_Child2");
+            Assert.AreEqual(vm.Pages[1].SubPages[0].ID, "ID_Child21");
+            Assert.AreEqual(vm.Pages[2].ID, "ID_Child3");
         }
 
         [Test]
-        public async Task GivenQueryAndEmptySignatureRepo_WhenHandle_ThenVMCorrect()
+        public async Task Handle_WhenSignatureRepoEmpty()
         {
             // Arrange
             var signatureRepository = Substitute.For<IRepository<TemplateSignature>>();
@@ -69,8 +108,14 @@ namespace Practicer.App.Tests
                 .GetBy(AppConfiguration.TemplateSignatureListID)
                 .Returns(Result.SuccessTask());
 
+            var templatePageRepository = Substitute.For<IRepository<TemplatePage>>();
+            templatePageRepository
+                .GetBy(AppConfiguration.TemplatePageRootID)
+                .Returns(Result.SuccessTask(Page));
+
             // Act
-            var result = await CreateHandler(signatureRepository, signatureListRepository).Handle(Query);
+            var result = await CreateHandler(
+                signatureRepository, signatureListRepository, templatePageRepository).Handle(Query);
             var vm = result.Get();
 
             // Assert
